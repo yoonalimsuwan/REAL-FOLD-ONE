384279c7639f38a80cf37e329e26e8d9be841599
Yoon A Limsuwan (美美) <msps4u@gmail.com>
2026-07-12 12:39:02 +0700

    Update developer information in the header

--- structural_langevin_fold_v2.py
+++ structural_langevin_fold_v2.py
@@ -1,7 +1,7 @@
 # =============================================================================
 # LANGEVIN ADVANCED WITH STRUCTURAL CALCULUS
 # =============================================================================
-# Developer    : Yoon A Limsuwan / MSPS NETWORK
+# Developer    : PAI , Yoon A Limsuwan / MSPS NETWORK
 # License      : MIT
 # Year         : 2026
 # ORCID        : 0009-0008-2374-0788
